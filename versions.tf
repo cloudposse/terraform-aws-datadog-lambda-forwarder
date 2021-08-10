89aea638c711e34f13f0260ac557da18d4b27b56
PePe Amengual <jose.amengual@gmail.com>
2021-08-10 16:32:15 -0700

    First commit

--- versions.tf
+++ versions.tf
@@ -12,9 +12,12 @@ terraform {
       version = ">= 2.2"
     }
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
+        source  = "hashicorp/aws"
+        version = ">= 3.0"
     }
-
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.2.0"
+    }
   }
 }
