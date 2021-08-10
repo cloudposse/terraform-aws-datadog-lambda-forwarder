7e1c068882fb3a5104b34d6a2aa27af6ee9d08c0
PePe Amengual <jose.amengual@gmail.com>
2021-08-10 16:52:52 -0700

    First commit

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -30,6 +30,7 @@ module "cloudwatch-logs" {
 
 module "datadog_lambda_forwarder" {
   source                = "../.."
+  region                = var.region
   forwarder_log_enabled = true
   cloudwatch_forwarder_log_groups = {
     postgres = module.cloudwatch-logs.log_group_name
