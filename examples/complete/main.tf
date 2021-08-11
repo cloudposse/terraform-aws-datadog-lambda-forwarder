5bd6fe31ec5d1221c5f50294670dd94d84ef05d3
PePe Amengual <jose.amengual@gmail.com>
2021-08-10 17:17:09 -0700

    First commit

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -15,7 +15,7 @@ module "subnets" {
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = true
+  nat_gateway_enabled  = false
   nat_instance_enabled = false
 
   context = module.this.context
