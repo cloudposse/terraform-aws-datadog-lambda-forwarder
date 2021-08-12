2821f334e322f7d034136a13556f0c7b7e765a1a
PePe Amengual <jose.amengual@gmail.com>
2021-08-12 11:07:18 -0700

    Update lambda-log.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- lambda-log.tf
+++ lambda-log.tf
@@ -71,7 +71,7 @@ resource "aws_lambda_permission" "allow_s3_bucket" {
 }
 
 resource "aws_s3_bucket_notification" "s3_bucket_notification" {
-  for_each = local.enabled_s3_logs ? toset(var.s3_buckets) : []
+  for_each = local.s3_logs_enabled ? toset(var.s3_buckets) : []
   bucket   = each.key
 
   lambda_function {
