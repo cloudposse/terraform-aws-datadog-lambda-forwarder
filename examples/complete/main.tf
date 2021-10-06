da464d19b9e4ba70cedde69db23fe43e5da9afd1
aknysh <andriy.knysh@gmail.com>
2021-10-06 17:22:39 -0400

    Update module versions and README

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,4 +1,4 @@
-module "cloudwatch-logs" {
+module "cloudwatch_logs" {
   source  = "cloudposse/cloudwatch-logs/aws"
   version = "0.5.0"
 
@@ -6,13 +6,13 @@ module "cloudwatch-logs" {
   context = module.this.context
 }
 
-module "datadog_lambda_forwarder" {
+module "datadog_lambda_log_forwarder" {
   source = "../.."
 
   forwarder_log_enabled = true
 
   cloudwatch_forwarder_log_groups = {
-    postgres = module.cloudwatch-logs.log_group_name
+    postgres = module.cloudwatch_logs.log_group_name
   }
 
   dd_api_key_source = var.dd_api_key_source
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,9 +1,9 @@
 output "lambda_forwarder_log_function_arn" {
   description = "Datadog Lambda forwarder CloudWatch/S3 function ARN"
-  value       = module.datadog_lambda_forwarder.lambda_forwarder_log_function_arn
+  value       = module.datadog_lambda_log_forwarder.lambda_forwarder_log_function_arn
 }
 
 output "lambda_forwarder_log_function_name" {
   description = "Datadog Lambda forwarder CloudWatch/S3 function name"
-  value       = module.datadog_lambda_forwarder.lambda_forwarder_log_function_name
+  value       = module.datadog_lambda_log_forwarder.lambda_forwarder_log_function_name
 }
