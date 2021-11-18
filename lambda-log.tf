7e0e94e18221b37ccfe859874a0e27b68a3db552
nitrocode <nitrocode@users.noreply.github.com>
2021-11-17 18:24:56 -0600

    Update lambda-log.tf

--- lambda-log.tf
+++ lambda-log.tf
@@ -203,10 +203,10 @@ data "aws_cloudwatch_log_group" "cloudwatch_log_group" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_subscription_filter" {
-  for_each = data.aws_cloudwatch_log_group.cloudwatch_log_group
+  for_each = local.lambda_enabled && var.forwarder_log_enabled ? var.cloudwatch_forwarder_log_groups : {}
 
   name            = module.forwarder_log_label.id
-  log_group_name  = each.value.name
+  log_group_name  = data.aws_cloudwatch_log_group.cloudwatch_log_group[each.value.name].name
   destination_arn = aws_lambda_function.forwarder_log[0].arn
-  filter_pattern  = var.cloudwatch_forwarder_log_groups[each.value.name]
+  filter_pattern  = var.cloudwatch_forwarder_log_groups[each.value.name].filter_pattern
 }
