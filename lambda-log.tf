a0258e0bab9610aadbc445a4096273c65341917c
nitrocode <nitrocode@users.noreply.github.com>
2021-11-18 10:51:52 -0600

    Add nullable filter pattern

--- lambda-log.tf
+++ lambda-log.tf
@@ -208,5 +208,5 @@ resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_subscription_f
   name            = module.forwarder_log_label.id
   log_group_name  = data.aws_cloudwatch_log_group.cloudwatch_log_group[each.key].name
   destination_arn = aws_lambda_function.forwarder_log[0].arn
-  filter_pattern  = var.cloudwatch_forwarder_log_groups[each.key].filter_pattern
+  filter_pattern  = lookup(var.cloudwatch_forwarder_log_groups[each.key], "filter_pattern", null)
 }
