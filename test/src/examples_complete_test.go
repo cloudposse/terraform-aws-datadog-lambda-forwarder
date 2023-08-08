b21de54c5b4951b0a93310369c3bada6114fe9d2
Nuru <Nuru@users.noreply.github.com>
2023-08-08 01:50:40 -0700

    Bless git directory when testing

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -1,85 +1,102 @@
 package test
 
 import (
-	"regexp"
-	"strings"
-	"testing"
+  "os/exec"
+  "regexp"
+  "strings"
+  "testing"
 
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/util/runtime"
+  "github.com/gruntwork-io/terratest/modules/random"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/stretchr/testify/assert"
+  "k8s.io/apimachinery/pkg/util/runtime"
 )
 
 // Test the Terraform module in examples/complete using Terratest.
 func TestExamplesComplete(t *testing.T) {
-	// This module needs to be run inside a Git Repository, so we cannot run it in parallel
-	// t.Parallel()
-	randID := strings.ToLower(random.UniqueId())
-	attributes := []string{randID}
+  // This module needs to be run inside a Git Repository, so we cannot run it in parallel
+  // t.Parallel()
 
-	varFiles := []string{"fixtures.us-east-2.tfvars"}
+  // If running on a GitHub Action Runner, invoke the necessary blessing
+  cmd := exec.Command("bash", "-c", "[[ -d /__w/actions/actions ]] && git config --global --add safe.directory /__w/actions/actions || true")
+  var stdout strings.Builder
+  cmd.Stdout = &stdout
+  var stderr strings.Builder
+  cmd.Stderr = &stderr
 
-	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/complete",
-		Upgrade:      true,
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: varFiles,
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-		},
-	}
+  if err := cmd.Run(); err != nil {
+    t.Logf("Running command: %s", cmd.String())
+    t.Logf("command stdout: %s", stdout.String())
+    t.Logf("command stderr: %s", stderr.String())
+    t.Log(err)
+  }
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+  randID := strings.ToLower(random.UniqueId())
+  attributes := []string{randID}
 
-	// If Go runtime crushes, run `terraform destroy` to clean up any resources that were created
-	defer runtime.HandleCrash(func(i interface{}) {
-		defer terraform.Destroy(t, terraformOptions)
-	})
+  varFiles := []string{"fixtures.us-east-2.tfvars"}
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	terraform.InitAndApply(t, terraformOptions)
+  terraformOptions := &terraform.Options{
+    // The path to where our Terraform code is located
+    TerraformDir: "../../examples/complete",
+    Upgrade:      true,
+    // Variables to pass to our Terraform code using -var-file options
+    VarFiles: varFiles,
+    Vars: map[string]interface{}{
+      "attributes": attributes,
+    },
+  }
 
-	lambdaFunctionName := terraform.Output(t, terraformOptions, "lambda_forwarder_log_function_name")
-	assert.Equal(t, "eg-ue2-test-datadog-lambda-forwarder-"+randID+"-logs", lambdaFunctionName)
+  // At the end of the test, run `terraform destroy` to clean up any resources that were created
+  defer terraform.Destroy(t, terraformOptions)
+
+  // If Go runtime crushes, run `terraform destroy` to clean up any resources that were created
+  defer runtime.HandleCrash(func(i interface{}) {
+    defer terraform.Destroy(t, terraformOptions)
+  })
+
+  // This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+  terraform.InitAndApply(t, terraformOptions)
+
+  lambdaFunctionName := terraform.Output(t, terraformOptions, "lambda_forwarder_log_function_name")
+  assert.Equal(t, "eg-ue2-test-datadog-lambda-forwarder-"+randID+"-logs", lambdaFunctionName)
 }
 
 func TestExamplesCompleteDisabled(t *testing.T) {
-	// This module needs to be run inside a Git Repository, so we cannot run it in parallel
-	// t.Parallel()
-	randID := strings.ToLower(random.UniqueId())
-	attributes := []string{randID}
+  // This module needs to be run inside a Git Repository, so we cannot run it in parallel
+  // t.Parallel()
 
-	varFiles := []string{"fixtures.us-east-2.tfvars"}
+  randID := strings.ToLower(random.UniqueId())
+  attributes := []string{randID}
 
-	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/complete",
-		Upgrade:      true,
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: varFiles,
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-			"enabled":    false,
-		},
-	}
+  varFiles := []string{"fixtures.us-east-2.tfvars"}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+  terraformOptions := &terraform.Options{
+    // The path to where our Terraform code is located
+    TerraformDir: "../../examples/complete",
+    Upgrade:      true,
+    // Variables to pass to our Terraform code using -var-file options
+    VarFiles: varFiles,
+    Vars: map[string]interface{}{
+      "attributes": attributes,
+      "enabled":    false,
+    },
+  }
 
-	// If Go runtime crushes, run `terraform destroy` to clean up any resources that were created
-	defer runtime.HandleCrash(func(i interface{}) {
-		defer terraform.Destroy(t, terraformOptions)
-	})
+  // At the end of the test, run `terraform destroy` to clean up any resources that were created
+  defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	results := terraform.InitAndApply(t, terraformOptions)
+  // If Go runtime crushes, run `terraform destroy` to clean up any resources that were created
+  defer runtime.HandleCrash(func(i interface{}) {
+    defer terraform.Destroy(t, terraformOptions)
+  })
 
-	// Should complete successfully without creating or changing any resources.
-	// Extract the "Resources:" section of the output to make the error message more readable.
-	re := regexp.MustCompile(`Resources: [^.]+\.`)
-	match := re.FindString(results)
-	assert.Equal(t, "Resources: 0 added, 0 changed, 0 destroyed.", match, "Re-applying the same configuration should not change any resources")
+  // This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+  results := terraform.InitAndApply(t, terraformOptions)
+
+  // Should complete successfully without creating or changing any resources.
+  // Extract the "Resources:" section of the output to make the error message more readable.
+  re := regexp.MustCompile(`Resources: [^.]+\.`)
+  match := re.FindString(results)
+  assert.Equal(t, "Resources: 0 added, 0 changed, 0 destroyed.", match, "Re-applying the same configuration should not change any resources")
 }
