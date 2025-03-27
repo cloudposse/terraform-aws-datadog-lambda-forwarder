be1cadb78e5ae31e3de182d061571a09fcaa8ff8
TehesFR <thiebaud.schmittlin@swile.co>
2025-03-27 11:17:21 +0100

    feat: add architectures variable

--- lambda-vpc-logs.tf
+++ lambda-vpc-logs.tf
@@ -86,6 +86,7 @@ resource "aws_lambda_function" "forwarder_vpclogs" {
   handler                        = "lambda_function.lambda_handler"
   source_code_hash               = data.archive_file.forwarder_vpclogs[0].output_base64sha256
   runtime                        = var.lambda_runtime
+  architectures                  = var.lambda_architectures
   memory_size                    = var.lambda_memory_size
   timeout                        = var.lambda_timeout
   reserved_concurrent_executions = var.lambda_reserved_concurrent_executions
